1ec151eafdcd313cd21217961da87672451e7086
maestrogreat1 <ononiwugreat@yahoo.com>
2024-06-06 01:13:03 +0100

    get it right

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,11 +35,11 @@ export const Contact = () => {
       <div className={styles.link}>
         <form ref={form} onSubmit={sendEmail}>
           <label>Name</label>
-          <input type="text" name="user_name" placeholder="Enter your name"/>
+          <input type="text" name="user_name" placeholder="Enter your name" required/>
           <label>Email</label>
-          <input type="email" name="user_email" placeholder="Enter your email address"/>
+          <input type="email" name="user_email" placeholder="Enter your email address" required/>
           <label>Message</label>
-          <textarea name="message" placeholder="Tell us your requests"/>
+          <textarea name="message" placeholder="Tell us your requests" required/>
           <input type="submit" value="Send"/>
         </form>
       </div>
